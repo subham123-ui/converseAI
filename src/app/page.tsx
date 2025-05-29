24b6da2a88de80faa8821f9fe796953fe88c5de8
subham123-ui <suvam.rockstar@gmail.com>
2025-05-29 23:29:16 +0530

    database-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 const Home = () => {
-  return <Button>Hello World</Button>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      { name, email, password },
+      {
+        onSuccess: () => {
+          //redirect to the dashboard or sign in page
+          window.alert("User created successfully, please sign in.");
+        },
+        onError: () => {
+          // display the error message
+          window.alert("Something went wrong, please try again.");
+        },
+      }
+    );
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-y-4">
+      <Input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 };
 
 export default Home;
